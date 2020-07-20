616286427e037fb723310195e7608446a1c7e722
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-07-21 00:20:28 +0600

    Get input element

--- app.js
+++ app.js
@@ -6,14 +6,37 @@ var budgetController = (function () {
 
 // UI controller
 var UIController = (function () {
-
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  }
 })();
 
 
 // App controller
 var controller = (function (budgetCtrl, UICtrl) {
-  var ctrlAddItem = function() {
+  var DOM = UICtrl.getDOMStrings();
+
+
+  var ctrlAddItem = function () {
     // get field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+
 
     // add item to the budget controller
 
@@ -24,14 +47,14 @@ var controller = (function (budgetCtrl, UICtrl) {
     // display the budget on the UI
 
   }
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
   document.addEventListener('keypress', function (event) {
-    
+
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
     }
-    
+
   });
 
 })(budgetController, UIController);
